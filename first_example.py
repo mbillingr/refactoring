a00468c2a7df7977718f0f72beb7b08a8573d131
mbilling <mbillingr@users.noreply.github.com>
2020-10-05 12:46:26 +0200

    step 1a: extract function (106)

--- first_example.py
+++ first_example.py
@@ -6,19 +6,7 @@ def statement(invoice, plays):
 
     for perf in invoice['performances']:
         play = plays[perf['playID']]
-        this_amount = 0
-
-        if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
-            this_amount += 300 * perf['audience']
-        else:
-            raise ValueError(f"unknown type: {play['type']}")
+        this_amount = amount_for(perf, play)
 
         # add volume credits
         volume_credits += max(perf['audience'] - 30, 0)
@@ -33,3 +21,19 @@ def statement(invoice, plays):
     result += f"Amount owed is {format(total_amount / 100)}\n"
     result += f"You earned {volume_credits} credits\n"
     return result
+
+
+def amount_for(perf, play):
+    this_amount = 0
+    if play['type'] == "tragedy":
+        this_amount = 40000
+        if perf['audience'] > 30:
+            this_amount += 1000 * (perf['audience'] - 30)
+    elif play['type'] == "comedy":
+        this_amount = 30000
+        if perf['audience'] > 20:
+            this_amount += 10000 + 500 * (perf['audience'] - 20)
+        this_amount += 300 * perf['audience']
+    else:
+        raise ValueError(f"unknown type: {play['type']}")
+    return this_amount
