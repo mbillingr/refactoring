650da6d3e5ed72a3993951ab5bd9d39a320b0786
mbilling <mbillingr@users.noreply.github.com>
2020-10-05 14:21:07 +0200

    step 8: implement html renderer

--- statement.py
+++ statement.py
@@ -16,5 +16,22 @@ def render_plain_text(data):
     return result
 
 
+def html_statement(invoice, plays):
+    return render_plain_text(create_statement_data(invoice, plays))
+
+
+def render_html(data):
+    result = f"<h1>Statement for {data['customer']}</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
+    for perf in data['performances']:
+        result += f"  <tr><td>{perf['play']['name']}</td><td>{perf['audience']}</td>"
+        result += f"<td>{usd(perf['amount'])}</td></tr>\n"
+    result += "</table>\n"
+    result += f"<p>Amount owed is <em>{usd(data['total_amount'])}</em></p>\n"
+    result += f"<p>You earned <em>{data['total_volume_credits']}</em> credits</p>\n"
+    return result
+
+
 def usd(cents):
     return "${:,.2f}".format(cents / 100)
